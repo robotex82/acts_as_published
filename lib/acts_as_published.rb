3aa939a9ae24b10dbc2fb21c753be8fce23271af
Roberto Vasquez Angel <robotex@robotex.de>
2012-08-18 22:15:02 +0200

    Added missing rails engine require.

--- lib/acts_as_published.rb
+++ lib/acts_as_published.rb
@@ -1,4 +1,5 @@
 require 'acts_as_published/active_admin_helper'
 require 'acts_as_published/active_record'
+require 'acts_as_published/engine'
 
 ActiveAdmin::ResourceDSL.send(:include, ActsAsPublished::ActiveAdminHelper) if defined?(ActiveAdmin::ResourceDSL)
