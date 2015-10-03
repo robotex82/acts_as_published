4ce792ad9e63ef753b75ec9790b9616a9cff3e2d
Roberto Vasquez Angel <robotex@robotex.de>
2015-10-03 17:30:34 +0200

    Removed rails restriction to v3.2 Added scopes

--- acts_as_published.gemspec
+++ acts_as_published.gemspec
@@ -15,33 +15,33 @@ Gem::Specification.new do |s|
 
   s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.rdoc"]
 
-  s.add_dependency "rails", "~> 3.2.7"
-  
+  s.add_dependency "rails", ">= 3.2.0", "< 5.0.0"
+
   # Module dependencies
 
   # Development Database
   s.add_development_dependency "sqlite3"
-  
+
   # Development Server
   s.add_development_dependency "thin"
-  
+
   # Documentation
   s.add_development_dependency "yard"
-  
+
   # Active admin
   s.add_development_dependency 'sass-rails'
-  s.add_development_dependency "coffee-rails"  
-  
+  s.add_development_dependency "coffee-rails"
+
   # Tests
-  s.add_development_dependency "capybara"    
+  s.add_development_dependency "capybara"
   s.add_development_dependency "rspec-rails", "~> 2.0"
   s.add_development_dependency "factory_girl_rails", "~> 1.0"
-  s.add_development_dependency 'ffaker'  
+  s.add_development_dependency 'ffaker'
 #  s.add_development_dependency 'guard-rails'
   s.add_development_dependency 'guard-rspec'
-  s.add_development_dependency 'guard-bundler'  
+  s.add_development_dependency 'guard-bundler'
   s.add_development_dependency "shoulda-matchers"
-  
+
   # Example data generation
-  s.add_development_dependency 'forgery', '0.5.0'  
+  s.add_development_dependency 'forgery', '0.5.0'
 end
--- lib/acts_as_published/active_record.rb
+++ lib/acts_as_published/active_record.rb
@@ -4,33 +4,34 @@ module ActsAsPublished
       klass.class_eval do
         extend ClassMethods
         include InstanceMethods
-      end  
-    end  
-  
+      end
+    end
+
     module ClassMethods
       def acts_as_published
         attr_accessible :published # , :published_at
-        
+
         def published
           t = self.arel_table
           where(t[:published_at].not_eq(nil))
         end
-        
+
         def unpublished
           t = self.arel_table
           where(t[:published_at].eq(nil))
         end
       end
     end
-    
+
     module InstanceMethods
       def published
         !!self.published_at
-      end  
+      end
       alias :published? :published
-      
+
       def published=(published)
-        if ::ActiveRecord::ConnectionAdapters::Column.value_to_boolean(published)
+        if ::ActiveRecord::ConnectionAdapters::Column
+             .value_to_boolean(published)
           self.published_at = Time.zone.now # .to_date
           # write_attribute(:published_at, Time.zone.now) # .to_date
         else
@@ -38,7 +39,7 @@ module ActsAsPublished
           # write_attribute(:published_at, nil)
         end
       end
-      
+
       def toggle_published
         if self.published_at.nil?
           self.published_at = Time.zone.now # .to_date
@@ -46,13 +47,14 @@ module ActsAsPublished
         else
           self.published_at = nil
           # write_attribute(:published_at, nil)
-        end 
+        end
       end
-      
+
       def toggle_published!
         toggle_published
         save!
-      end 
+      end
     end
   end
-end  
+end
+
